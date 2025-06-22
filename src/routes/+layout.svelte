b2e362a44da53d09554d76152dac2206f2d89fad
Marco Miduri <mmiduri.job@gmail.com>
2025-06-22 20:25:55 +0200

    Update +layout.svelte with base url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
     import { fade } from 'svelte/transition'
-
+    import { base } from '$app/paths'
     const { children, data } = $props()
 </script>
 
 <nav>
     <ul>
         <li>
-            <a href="/">Home</a>
+            <a href="{base}/">Home</a>
         </li>
         <li>
-            <a href="/works">Works</a>
+            <a href="{base}/works">Works</a>
         </li>
 
         <li>
-            <a href="/crochet">Crochet</a>
+            <a href="{base}/crochet">Crochet</a>
         </li>
     </ul>
 </nav>
