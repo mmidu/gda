e9c00518da4651aeeaaa9bc85fcd3b104a85aa41
Marco Miduri <mmiduri.job@gmail.com>
2025-06-22 18:52:48 +0200

    First commit.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { onNavigate } from "$app/navigation"
+    const { children } = $props()
+
+    onNavigate((navigation) => {
+        if (!document.startViewTransition) return
+
+        return new Promise((resolve) => {
+            document.startViewTransition(async () => {
+                resolve()
+                await navigation.complete
+            })
+        })
+    })
+</script>
+
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/works">Works</a>
+        </li>
+
+        <li>
+            <a href="/crochet">Crochet</a>
+        </li>
+    </ul>
+</nav>
+
+{@render children()}
