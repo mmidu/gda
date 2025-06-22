e04684de44726ace76e8e2f8a5b1ee8c85a20a75
Marco Miduri <mmiduri.job@gmail.com>
2025-06-22 19:19:18 +0200

    Add fade transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,12 @@
 <script lang="ts">
-	import { onNavigate } from "$app/navigation"
-    const { children } = $props()
+    export const prerender = true
+    import { fade } from 'svelte/transition'
 
-    onNavigate((navigation) => {
-        if (!document.startViewTransition) return
-
-        return new Promise((resolve) => {
-            document.startViewTransition(async () => {
-                resolve()
-                await navigation.complete
-            })
-        })
-    })
+    const { children, data } = $props()
 </script>
 
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/works">Works</a>
-        </li>
-
-        <li>
-            <a href="/crochet">Crochet</a>
-        </li>
-    </ul>
-</nav>
-
-{@render children()}
+{#key data.currentRoute}
+	<main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+		{@render children()}
+	</main>
+{/key}
