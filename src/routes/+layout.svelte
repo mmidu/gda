704e80268b9079e26db2219c7a945539ec1f8005
Marco Miduri <mmiduri.job@gmail.com>
2025-06-22 19:36:59 +0200

    Add configuration for SSG with dockerised nginx server.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
-    export const prerender = true
     import { fade } from 'svelte/transition'
 
     const { children, data } = $props()
 </script>
+
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/works">Works</a>
+        </li>
+
+        <li>
+            <a href="/crochet">Crochet</a>
+        </li>
+    </ul>
+</nav>
 
 {#key data.currentRoute}
 	<main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,6 @@
+export const prerender = true
+export const trailingSlash = 'always'
+
 export const load = ({ url }) => {
 	const currentRoute = url.pathname
 
