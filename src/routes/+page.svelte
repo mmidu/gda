a048aab60903ef3ca62e245d3859f031beeb1693
Marco Miduri <mmiduri.job@gmail.com>
2025-06-25 22:04:03 +0200

    Hide logo on click.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,54 @@
 <script lang="ts">
     import logo from '$lib/assets/logo.png';
+    let hide = false
+
+    function handleClick() {
+        hide = true
+    }
+
 </script>
 
-<div class="logo">
+
+<button class={{ logo, hide }} on:click={handleClick}>
     <img src={logo} alt="Logo Giulia d'Alessandro" />
-</div>
+</button>
 
 <style lang="scss">
 .logo {
+    all: unset;
     margin: 0;
     padding: 0;
     display: flex;
     align-items: center;
     justify-content: center;
     height: 100vh;
+    width: 100%;
+    opacity: 1;
+    animation: fadeIn 1.5s ease-in-out;
+
     img {
         max-width: 300px;
         cursor: pointer;
-        animation: fadeIn 1.5s ease-in-out;
     }
 }
+.hide {
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity 1.5s ease-in-out;
+
+}
+
+
+@keyframes fadeOut {
+    from { opacity: 1; }
+    to { opacity: 0; }
+}
+
+@keyframes hide {
+    from { display: block; }
+    to { display: block; }
+}
+
 @keyframes fadeIn {
     from { opacity: 0; }
     to { opacity: 1; }
