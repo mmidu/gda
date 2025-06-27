8eb27712cbdf0c6760daead6d7b947ee33ed9941
Marco Miduri <mmiduri.job@gmail.com>
2025-06-27 20:54:29 +0200

    Add crochet page header.

--- src/routes/crochet/+page.svelte
+++ src/routes/crochet/+page.svelte
@@ -1,1 +1,15 @@
-<h1>Crochet</h1>
+<header class={['header']}>
+    <h1>Crochet</h1>
+</header>
+
+<style lang="scss">
+    .header {
+        border-bottom: 1px solid #7a6e66;
+        text-align: end;
+        padding: 1rem 2rem;
+
+        h1 {
+            margin: 0;
+        }
+    }
+</style>
