1ebd2603b91f197dc5131c2c5cc485ce6877fcb9
Marco Miduri <mmiduri.job@gmail.com>
2025-06-27 21:06:06 +0200

    Add initial crochet images.

---
+++ src/components/Grid.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let data : string[]
+</script>
+<ul>
+    {#each data as src}
+        <img src={src as string} alt={'alt'}/>
+    {/each}
+    <li>uno</li>
+    <li>due</li>
+    <li>tre</li>
+</ul>
--- src/routes/crochet/+page.svelte
+++ src/routes/crochet/+page.svelte
@@ -1,6 +1,15 @@
+<script>
+	import Grid from "../../components/Grid.svelte"
+
+    const crochetImages = Object.keys(import.meta.glob(['$lib/assets/crochet/**.jpeg'], { eager: true, query: '?url' }));
+
+</script>
 <header class={['header']}>
     <h1>Crochet</h1>
 </header>
+<section>
+    <Grid data={crochetImages}/>
+</section>
 
 <style lang="scss">
     .header {
